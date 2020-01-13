d9abafbf9ef45af5cb8f3d9e5cc4f67bbe96de98
kristinacarlsen <kristida84@gmail.com>
2020-01-13 13:01:08 -0700

    adds heroku

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 require('dotenv').config();
 
@@ -49,6 +50,15 @@ app.post('/api/form', (req, res) => {
         });
 
     });
+
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 3001
 
