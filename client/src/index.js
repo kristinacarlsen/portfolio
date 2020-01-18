88690aaf41440c6f0b6340d54886af5e5c47b329
Aharr <habibullah.aharrfaris@gmail.com>
2020-01-18 12:35:03 +0600

    after a new folder structure and directory

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from 'axios';
+
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 import App from "./App";
-import "./index.css";
+
+axios.defaults.baseURL = 'http://localhost:3001'; // comment out this line on production
+// axios.defaults.baseURL = 'UNCOMMENT THIS and put URL of your site on production';
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
