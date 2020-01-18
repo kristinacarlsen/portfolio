8dd4a6cc63a9e037d716b17c2f43bdaafd5a27a1
kristinacarlsen <kristida84@gmail.com>
2020-01-18 10:08:45 -0700

    changes to axios request

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import App from "./App";
+Axios.defaults.baseURL = "http://localhost:3000";
 import "./index.css";
+import Axios from "axios";
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 app.all("/", function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
