fc4e800eb9c7079b9df93c118e5e60b46fe69e4f
kristinacarlsen <kristida84@gmail.com>
2020-01-20 22:17:18 -0700

    updates build

--- server/index.js
+++ server/index.js
@@ -55,13 +55,9 @@ app.post("/api/form", async (req, res) => {
   await res.status(200).json({ msg: "email sent" });
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 3001;
 
